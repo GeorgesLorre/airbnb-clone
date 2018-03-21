19faad8940fa37311c0fc2dda0e9864f5cb17ee8
GeorgesLorre <mr.lorre@gmail.com>
2018-03-21 15:00:50 +0100

    picture upload for a service

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320163452) do
+ActiveRecord::Schema.define(version: 20180321133355) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20180320163452) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.string "photo"
     t.index ["contractor_id"], name: "index_services_on_contractor_id"
   end
 
